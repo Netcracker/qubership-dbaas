0844a3288412115f1cf8b6e69ba4433bb62cea0c
Evgenii Stetsenko <i@estetsenko.com>
2025-07-08 10:43:59 +0300

    feat(tests): migrate to JUnit 5

--- dbaas/encryption-services-cipher/src/test/java/org/qubership/cloud/encryption/cipher/build/DecryptionRequestBuilderTest.java
+++ dbaas/encryption-services-cipher/src/test/java/org/qubership/cloud/encryption/cipher/build/DecryptionRequestBuilderTest.java
@@ -1,6 +1,7 @@
 package org.qubership.cloud.encryption.cipher.build;
 
 import org.junit.jupiter.api.Test;
+import org.qubership.cloud.encryption.cipher.DecryptionRequest;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -11,6 +12,16 @@ class DecryptionRequestBuilderTest {
                 NullPointerException.class,
                 () -> DecryptionRequestBuilder.createBuilder().build(),
                 "EncryptionRequestBuilder have required field it plain text without it build method should fail"
+        );
+    }
+
+    @Test
+    void testUseBuilderAsResultAndGetRequiredEncryptedTextLeadToNPE() {
+        DecryptionRequest result = (DecryptionRequest) DecryptionRequestBuilder.createBuilder();
+        assertThrows(
+                NullPointerException.class,
+                result::getEncryptedText,
+                "empty array it empty string, but in case if value was not specify we should fail with NPE"
         );
     }
 }
