e0cfba47ae58d34035cbd02331fe01d79a1f80bb
Alisher Askar <alisherask2005@gmail.com>
2025-08-15 16:44:37 +0500

    feat: add validation annotations to dto classes

--- dbaas/dbaas-aggregator/src/main/java/org/qubership/cloud/dbaas/dto/backupV2/BackupResponse.java
+++ dbaas/dbaas-aggregator/src/main/java/org/qubership/cloud/dbaas/dto/backupV2/BackupResponse.java
@@ -1,5 +1,7 @@
 package org.qubership.cloud.dbaas.dto.backupV2;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
@@ -11,26 +13,39 @@ import java.util.List;
 @NoArgsConstructor
 @Schema(description = "Response containing backup operation details")
 public class BackupResponse {
+
+    @NotBlank
     @Schema(description = "Unique identifier of the backup", example = "before-prod-update-20251013T1345-G5s8", required = true)
     private String backupName;
+
+    @NotBlank
     @Schema(description = "Name of the storage backend containing the backup", example = "s3-backend", required = true)
     private String storageName;
+
+    @NotBlank
     @Schema(description = "Path to the backup file in the storage", example = "/backups", required = true)
     private String blobPath;
+
     @Schema(
             description = "How to handle external databases during backup",
             example = "fail",
             implementation = ExternalDatabaseStrategy.class
     )
+    @NotNull
     private ExternalDatabaseStrategy externalDatabaseStrategy;
+
+    @NotNull
     @Schema(description = "Whether external databases were skipped during the backup", example = "false")
     private boolean ignoreNotBackupableDatabases;
+
     @Schema(
             description = "Filter criteria"
     )
     private FilterCriteria filterCriteria;
+
     @Schema(description = "Status of the backup operation")
     private BackupStatusResponse status;
+
     @Schema(
             description = "List of logical backups"
     )
