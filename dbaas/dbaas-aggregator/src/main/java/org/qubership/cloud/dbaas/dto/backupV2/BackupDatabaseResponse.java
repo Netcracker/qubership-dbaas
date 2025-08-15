39105b82bd91b46810b6a44673fc19a7119d9e59
Alisher Askar <alisherask2005@gmail.com>
2025-08-15 16:47:59 +0500

    feat: delete id field from BackupDatabaseResponse.class

--- dbaas/dbaas-aggregator/src/main/java/org/qubership/cloud/dbaas/dto/backupV2/BackupDatabaseResponse.java
+++ dbaas/dbaas-aggregator/src/main/java/org/qubership/cloud/dbaas/dto/backupV2/BackupDatabaseResponse.java
@@ -9,14 +9,12 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
-import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @Schema(description = "Logical database backup details")
 public class BackupDatabaseResponse {
-    @Schema(description = "Unique identifier of the logical backup", example = "4e1e9922-e5bf-490d-bb57-18a14de593c6", required = true)
-    private UUID id;
+
     @Schema(description = "Name of the database", example = "mydb", required = true)
     private String name;
     @Schema(description = "List of database classifiers")
