70b0092e9d665c3fa6515e3be2b3e0d820a12c22
Alisher Askar <alisherask2005@gmail.com>
2025-08-21 18:03:19 +0500

    feat: entity, repository, converter of restore tables

---
+++ dbaas/dbaas-aggregator/src/main/java/org/qubership/cloud/dbaas/entity/pg/backupV2/LogicalRestoreStatus.java
@@ -0,0 +1,37 @@
+package org.qubership.cloud.dbaas.entity.pg.backupV2;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LogicalRestoreStatus {
+
+    private Status status = Status.NOT_STARTED;
+    private String errorMessage;
+    private LocalDateTime creationTime;
+    private LocalDateTime completionTime;
+
+    private List<LogicalBackupStatus.Database> databases = new ArrayList<>();
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Database{
+        private String databaseName;
+        private Status status;
+        private Integer size;
+        private String duration;
+        private String path;
+        private String errorMessage;
+    }
+}
