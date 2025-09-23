9cf01cc43038dc6f2837c7420decc6c4f82ddaed
Artem Kuleshov <artem.kuleshov@netcracker.com>
2025-09-23 14:33:40 +0400

    refactor: remove tls-utils dependency and read TLS params directly

--- dbaas/dbaas-aggregator/src/main/java/com/netcracker/cloud/dbaas/JdbcUtils.java
+++ dbaas/dbaas-aggregator/src/main/java/com/netcracker/cloud/dbaas/JdbcUtils.java
@@ -1,6 +1,5 @@
 package com.netcracker.cloud.dbaas;
 
-import com.netcracker.cloud.security.core.utils.tls.TlsUtils;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,22 +17,26 @@ import java.util.Map;
 @Slf4j
 public class JdbcUtils {
 
-    private static final String SSL_URL_PARAMS = "?ssl=true&sslfactory=org.postgresql.ssl.SingleCertValidatingFactory&sslfactoryarg=";
+    private static final String CERTIFICATE_STORE_PATH = getEnvOrProperty("CERTIFICATE_FILE_PATH", "/etc/tls");
+    private static final String CA_CERTIFICATE_URL = "file://" + CERTIFICATE_STORE_PATH + "/ca.crt";
+    private static final String SSL_URL_PARAMS = "?ssl=true&sslfactory=org.postgresql.ssl.SingleCertValidatingFactory&sslfactoryarg=" + CA_CERTIFICATE_URL;
 
     public static String buildConnectionURL(String pgHost, String pgPort, String pgDatabase) {
         String url = String.format("jdbc:postgresql://%s:%s/%s", pgHost, pgPort, pgDatabase);
         if (isInternalTlsEnabled()) {
             log.debug("Going to use secured connection to postgres");
-            String rootCertificatePath = "file://" + TlsUtils.getCaCertificatePath();
-            url += SSL_URL_PARAMS + rootCertificatePath;
+            url += SSL_URL_PARAMS;
         }
         log.debug("Using not secured connection to postgres");
         return url;
     }
 
     private static boolean isInternalTlsEnabled() {
-        String internalTlsEnabled = System.getenv("INTERNAL_TLS_ENABLED");
-        return Boolean.parseBoolean(internalTlsEnabled);
+        return Boolean.parseBoolean(getEnvOrProperty("INTERNAL_TLS_ENABLED", "false"));
+    }
+
+    private static String getEnvOrProperty(String name, String defaultValue) {
+        return System.getProperty(name, System.getenv().getOrDefault(name, defaultValue));
     }
 
     public static List<Map<String, Object>> queryForList(Connection connection, String query) throws SQLException {
