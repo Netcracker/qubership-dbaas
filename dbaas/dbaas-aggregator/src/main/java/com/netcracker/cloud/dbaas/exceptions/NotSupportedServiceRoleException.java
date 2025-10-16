d5f19e99d2b230fd0992186d31b2a4490d58e844
Artem Kuleshov <artem.kuleshov@netcracker.com>
2025-10-16 16:06:27 +0500

    feat: improve error message for NotSupportedServiceRoleException (#152)

--- dbaas/dbaas-aggregator/src/main/java/com/netcracker/cloud/dbaas/exceptions/NotSupportedServiceRoleException.java
+++ dbaas/dbaas-aggregator/src/main/java/com/netcracker/cloud/dbaas/exceptions/NotSupportedServiceRoleException.java
@@ -3,11 +3,7 @@ package com.netcracker.cloud.dbaas.exceptions;
 import com.netcracker.cloud.core.error.runtime.ErrorCodeException;
 
 public class NotSupportedServiceRoleException extends ErrorCodeException {
-    public NotSupportedServiceRoleException(String role) {
-        super(ErrorCodes.CORE_DBAAS_4023, ErrorCodes.CORE_DBAAS_4023.getDetail(role));
-    }
-
-    public NotSupportedServiceRoleException() {
-        super(ErrorCodes.CORE_DBAAS_4023, ErrorCodes.CORE_DBAAS_4023.getDetail(""));
+    public NotSupportedServiceRoleException(String role, String ownerService, String requesterService) {
+        super(ErrorCodes.CORE_DBAAS_4023, ErrorCodes.CORE_DBAAS_4023.getDetail(role, ownerService, requesterService));
     }
 }
