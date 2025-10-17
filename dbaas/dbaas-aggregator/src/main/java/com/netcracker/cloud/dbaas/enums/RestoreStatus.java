c9ae1ad06b03b52412f8da1b4084bb45ef78478b
Alisher Askar <alisherask2005@gmail.com>
2025-10-17 13:07:13 +0500

    feat: delete pending status

--- dbaas/dbaas-aggregator/src/main/java/com/netcracker/cloud/dbaas/enums/RestoreStatus.java
+++ dbaas/dbaas-aggregator/src/main/java/com/netcracker/cloud/dbaas/enums/RestoreStatus.java
@@ -1,5 +1,5 @@
 package com.netcracker.cloud.dbaas.enums;
 
 public enum RestoreStatus {
-    NOT_STARTED, PENDING, IN_PROGRESS, FAILED, COMPLETED, DELETED
+    NOT_STARTED, IN_PROGRESS, FAILED, COMPLETED, DELETED
 }
