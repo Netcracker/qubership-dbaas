84a5bf3a5e4adb8912a91088a86cbab185ab00dd
Alisher Askar <alisherask2005@gmail.com>
2025-11-13 15:53:22 +0500

    feat: fix path

---
+++ dbaas/dbaas-aggregator/src/main/java/com/netcracker/cloud/dbaas/repositories/pg/jpa/BackupDatabaseRepository.java
@@ -0,0 +1,13 @@
+package com.netcracker.cloud.dbaas.repositories.pg.jpa;
+
+import com.netcracker.cloud.dbaas.entity.pg.backupV2.BackupDatabase;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+
+import java.util.UUID;
+
+@Transactional
+@ApplicationScoped
+public class BackupDatabaseRepository implements PanacheRepositoryBase<BackupDatabase, UUID> {
+}
