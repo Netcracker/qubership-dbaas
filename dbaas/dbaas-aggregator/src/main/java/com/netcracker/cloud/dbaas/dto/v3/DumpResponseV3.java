2aab3b9af500050a1b1ffcd58386dfe7b91fec55
Alisher Askar <alisherask2005@gmail.com>
2026-02-27 17:41:58 +0500

    fix: hide entities from OpenApi spec

--- dbaas/dbaas-aggregator/src/main/java/com/netcracker/cloud/dbaas/dto/v3/DumpResponseV3.java
+++ dbaas/dbaas-aggregator/src/main/java/com/netcracker/cloud/dbaas/dto/v3/DumpResponseV3.java
@@ -9,6 +9,8 @@ import com.netcracker.cloud.dbaas.entity.pg.backupV2.Restore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import java.util.List;
 
@@ -16,12 +18,18 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DumpResponseV3 {
-
+    @Schema(implementation = Object.class, type = SchemaType.ARRAY)
     private DumpRulesV3 rules;
+    @Schema(implementation = Object.class, type = SchemaType.ARRAY)
     private List<Database> logicalDatabases;
+    @Schema(implementation = Object.class, type = SchemaType.ARRAY)
     private List<PhysicalDatabase> physicalDatabases;
+    @Schema(implementation = Object.class, type = SchemaType.ARRAY)
     private List<DatabaseDeclarativeConfig> declarativeConfigurations;
+    @Schema(implementation = Object.class, type = SchemaType.ARRAY)
     private List<BgDomain> blueGreenDomains;
+    @Schema(implementation = Object.class, type = SchemaType.ARRAY)
     private List<Backup> backups;
+    @Schema(implementation = Object.class, type = SchemaType.ARRAY)
     private List<Restore> restores;
 }
