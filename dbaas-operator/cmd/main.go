0abb6204a02a9c55409437c5d0665495c988d72c
Abhimanyu Roy <51357041+TheHollowMonarch@users.noreply.github.com>
2026-04-07 16:33:32 +0530

    fix: setting the ctrl to point to the defined logger (#363)
    * fix: setting the ctrl to point to the defined logger
    
    * fix: adding the interface to satisfy set logger
    
    * fix: passing the name parameter to the logs before controller name

--- dbaas-operator/cmd/main.go
+++ dbaas-operator/cmd/main.go
@@ -99,6 +99,8 @@ func main() {
 		"Maximum delay cap for exponential backoff on reconcile errors.")
 	flag.Parse()
 
+	ctrl.SetLogger(newLogrLogger("dbaas-operator"))
+
 	// if the enable-http2 flag is false (the default), http/2 should be disabled
 	// due to its vulnerabilities. More specifically, disabling http/2 will
 	// prevent from being vulnerable to the HTTP/2 Stream Cancellation and
