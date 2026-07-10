b4262a5f1934d2d8fe3d1bff45786152c4f20679
Artem Kuleshov <artem.kuleshov@netcracker.com>
2026-07-10 15:06:23 +0500

    perf: calculate properties only once
    Anyway Agroal won't reread the connection properties in runtime.

--- dbaas/dbaas-aggregator/src/main/java/com/netcracker/cloud/dbaas/config/PostgresConnectionConfigSource.java
+++ dbaas/dbaas-aggregator/src/main/java/com/netcracker/cloud/dbaas/config/PostgresConnectionConfigSource.java
@@ -8,40 +8,34 @@ import java.util.Map;
 import java.util.Set;
 
 public class PostgresConnectionConfigSource implements ConfigSource {
+    private final Map<String, String> properties = new HashMap<>();
 
-    private static final Set<String> PROPERTY_NAMES = Set.of(
-        "quarkus.datasource.jdbc.url",
-        "quarkus.datasource.username",
-        "quarkus.datasource.password");
+    public PostgresConnectionConfigSource() {
+        properties.put("quarkus.datasource.jdbc.url", JdbcUtils.resolveConnectionURL());
+        properties.put("quarkus.datasource.username", JdbcUtils.resolveUsername());
+        properties.put("quarkus.datasource.password", JdbcUtils.resolvePassword());
+    }
 
     @Override
     public Map<String, String> getProperties() {
-        return resolve();
+        return properties;
     }
 
     @Override
     public Set<String> getPropertyNames() {
-        return PROPERTY_NAMES;
+        return properties.keySet();
     }
 
     @Override
     public String getValue(String propertyName) {
-        if (!PROPERTY_NAMES.contains(propertyName)) {
+        if (!getPropertyNames().contains(propertyName)) {
             return null;
         }
-        return resolve().get(propertyName);
+        return getProperties().get(propertyName);
     }
 
     @Override
     public String getName() {
         return "PostgresConnectionConfigSource";
-    }
-
-    private Map<String, String> resolve() {
-        Map<String, String> properties = new HashMap<>();
-        properties.put("quarkus.datasource.jdbc.url", JdbcUtils.resolveConnectionURL());
-        properties.put("quarkus.datasource.username", JdbcUtils.resolveUsername());
-        properties.put("quarkus.datasource.password", JdbcUtils.resolvePassword());
-        return Map.copyOf(properties);
     }
 }
