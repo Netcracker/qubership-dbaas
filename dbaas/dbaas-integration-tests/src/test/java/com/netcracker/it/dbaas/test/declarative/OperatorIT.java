eb26a41087e8baced45d0fdad4d65901eeffa9a4
Abhimanyu Roy <abhimanyuroy3197@gmail.com>
2026-08-11 16:51:13 +0500

    fix: reuse settings test for PostgreSQL extensions

--- dbaas/dbaas-integration-tests/src/test/java/com/netcracker/it/dbaas/test/declarative/OperatorIT.java
+++ dbaas/dbaas-integration-tests/src/test/java/com/netcracker/it/dbaas/test/declarative/OperatorIT.java
@@ -755,33 +755,6 @@ public class OperatorIT extends AbstractIT {
                 }
 
                 @Test
-                void testInternalDatabaseWithPostgresqlExtensions() throws IOException, SQLException {
-                    String crName = generateName();
-                    String microserviceName = generateName();
-                    Map<String, Object> settings = Map.of("pgExtensions", List.of("uuid-ossp"));
-
-                    var cr = buildInternalDatabaseCR(crName, microserviceName, NAMESPACE, "postgresql", false,
-                            Map.of("settings", settings));
-
-                    createCR(CRD_INTERNAL_DATABASE, cr);
-                    waitForDesiredState(CRD_INTERNAL_DATABASE, cr,
-                            PHASE_SUCCEEDED, STATUS_TRUE, REASON_DATABASE_PROVISIONED, STATUS_FALSE);
-
-                    DatabaseResponse database = helperV3.getDatabaseByClassifierAsPOJO(
-                            helperV3.getClusterDbaAuthorization(),
-                            new ClassifierBuilder().ms(microserviceName).ns(NAMESPACE).build(),
-                            NAMESPACE, "postgresql", 200);
-                    assertEquals(settings, database.getSettings());
-
-                    try (var connection = helperV3.connectPg(database);
-                         var statement = connection.createStatement();
-                         var extensions = statement.executeQuery(
-                                 "select 1 from pg_extension where extname = 'uuid-ossp'")) {
-                        assertTrue(extensions.next(), "The uuid-ossp extension must be installed");
-                    }
-                }
-
-                @Test
                 void testInternalDatabaseClassifierNamespaceMismatch() throws IOException {
                     String crName = generateName();
                     String microserviceName = generateName();
@@ -993,7 +966,7 @@ public class OperatorIT extends AbstractIT {
                 }
 
                 @Test
-                void testInternalDatabaseCanUpdateSettings() throws IOException {
+                void testInternalDatabaseCanUpdateSettings() throws IOException, SQLException {
                     String crName = generateName();
                     String microserviceName = generateName();
 
@@ -1002,7 +975,9 @@ public class OperatorIT extends AbstractIT {
                     waitForDesiredState(CRD_INTERNAL_DATABASE, cr,
                             PHASE_SUCCEEDED, STATUS_TRUE, REASON_DATABASE_PROVISIONED, STATUS_FALSE);
 
-                    var updatedSettings = Map.of(TEST_ID, "updated");
+                    var updatedSettings = Map.of(
+                            TEST_ID, "updated",
+                            "pgExtensions", List.of("uuid-ossp"));
                     var updatedResource = kubernetesClient.genericKubernetesResources(CRD_INTERNAL_DATABASE)
                             .inNamespace(NAMESPACE)
                             .resource(cr)
@@ -1020,6 +995,19 @@ public class OperatorIT extends AbstractIT {
                             .get();
                     var refreshedSpec = (Map<String, Object>) refreshed.getAdditionalProperties().get("spec");
                     assertEquals(updatedSettings, refreshedSpec.get("settings"));
+
+                    DatabaseResponse database = helperV3.getDatabaseByClassifierAsPOJO(
+                            helperV3.getClusterDbaAuthorization(),
+                            new ClassifierBuilder().ms(microserviceName).ns(NAMESPACE).build(),
+                            NAMESPACE, "postgresql", 200);
+                    assertEquals(updatedSettings, database.getSettings());
+
+                    try (var connection = helperV3.connectPg(database);
+                         var statement = connection.createStatement();
+                         var extensions = statement.executeQuery(
+                                 "select 1 from pg_extension where extname = 'uuid-ossp'")) {
+                        assertTrue(extensions.next(), "The uuid-ossp extension must be installed");
+                    }
                 }
             }
 
